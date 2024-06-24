1b1005a2774c4d34e1e58c2433205ded6ddee6f4
Aditya Pathak <adityap029@gmail.com>
2024-06-24 15:01:09 -0400

    Initial Commit

---
+++ LandmarksSwiftTutorial/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LandmarksSwiftTutorial
+//
+//  Created by Aditya Pathak on 6/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LandmarksSwiftTutorial/LandmarksSwiftTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksSwiftTutorialApp.swift
+//  LandmarksSwiftTutorial
+//
+//  Created by Aditya Pathak on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksSwiftTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
