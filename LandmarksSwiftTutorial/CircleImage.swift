5a341ef531daa076f189faec679ecb2656a37bd1
Aditya Pathak <adityap029@gmail.com>
2024-06-27 00:41:15 -0400

    Finished playing around with formatting, have a good grasp on the basics.

---
+++ LandmarksSwiftTutorial/CircleImage.swift
@@ -0,0 +1,18 @@
+//
+//  CircleImage.swift
+//  LandmarksSwiftTutorial
+//
+//  Created by Aditya Pathak on 6/27/24.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
--- LandmarksSwiftTutorial/ContentView.swift
+++ LandmarksSwiftTutorial/ContentView.swift
@@ -9,9 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Turtle Rock")
-            .font(.title)
-            .foregroundColor(.green)
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+            .foregroundColor(Color(red: 137.0 / 255.0, green: 207.0 / 255.0, blue: 240.0 / 255.0))
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
+        }
+        .padding()
     }
 }
 
