2d22daa6989629146968726cf7427e8ba050c3b4
Aditya Pathak <adityap029@gmail.com>
2024-06-26 23:21:36 -0400

    Played around with some text, learned how to add titles and add colors and stuff

--- LandmarksSwiftTutorial/ContentView.swift
+++ LandmarksSwiftTutorial/ContentView.swift
@@ -9,10 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello Twitter!")
+        Text("Turtle Rock")
+            .font(.title)
+            .foregroundColor(.green)
     }
 }
 
 #Preview {
     ContentView()
 }
+
+
+/*
+ 
+ Here is the color code for baby blue. For whatever reason, I need to divide each value by 255.0, I guess it's just some formatting thing in swift
+ 
+.foregroundColor(Color(red: 137.0 / 255.0, green: 207.0 / 255.0, blue: 240.0 / 255.0))
+*/
